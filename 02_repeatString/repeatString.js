5a25216ac78774a6508731d4e7af8447cb135d0a
gushags <jeff@hagkull.com>
2025-02-24 13:48:34 -0800

    Finish second exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  if (num > 0) {
+    arr = [string];
+    for (let i = 0; i < num - 1; i++) {
+      arr.push(string);
+    }
+    return arr.join("");
+  } else if (num < 0) {
+    return "ERROR";
+  } else {
+    return "";
+  }
 };
 
 // Do not edit below this line
