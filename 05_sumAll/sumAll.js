332f21a196d72a7ef67538fd1f3ed1c504eb6e5c
gushags <jeff@hagkull.com>
2025-02-24 21:22:14 -0800

    Passes all seven tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,29 @@
-const sumAll = function() {
+const sumAll = function (begin, end) {
+  let sum = 0;
+  let start;
+  let finish;
+  if (
+    begin < 0 ||
+    end < 0 ||
+    !Number.isInteger(begin) ||
+    !Number.isInteger(end)
+  ) {
+    return "ERROR";
+  }
 
+  if (begin > end) {
+    start = end;
+    finish = begin;
+  } else {
+    start = begin;
+    finish = end;
+  }
+  for (let i = start; i <= finish; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
+
 module.exports = sumAll;
