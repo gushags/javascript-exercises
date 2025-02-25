eee2155508790f406fd5c3fd388f7ecdac8a7a0b
gushags <jeff@hagkull.com>
2025-02-24 20:41:38 -0800

    Passes two tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function (arrTest, ...args) {
+  // find num in arr
+
+  for (let arg of args) {
+    for (let i = 0; i < arrTest.length; i++) {
+      if (arrTest[i] == arg) {
+        arrTest.splice(i, 1);
+      }
+    }
+
+    // Feels like this should be recursion, but not working
+    // if num is in array, run the for loop
+    // then check if num is in the new array
+  }
+  return arrTest;
 };
 
 // Do not edit below this line
