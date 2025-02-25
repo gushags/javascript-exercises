0fd60fdca3c77378189c6cf47e6607f733f124c6
gushags <jeff@hagkull.com>
2025-02-25 06:50:38 -0800

    Passes all seven tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  if (!(year % 4)) {
+    if (!(year % 100) && !(year % 400)) {
+      return true;
+    } else if (!(year % 100)) {
+      return false;
+    }
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
