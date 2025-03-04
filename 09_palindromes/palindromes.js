af0de2ecf03e3fae8a108c713b0b5759ac8025f9
gushags <jeff@hagkull.com>
2025-03-04 12:01:46 -0800

    Passes all tests -- ex 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,25 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  let test = str
+    .toUpperCase()
+    .split("")
+    .filter((char) => {
+      return /[a-zA-Z0-9]/.test(char);
+    })
+    .join("");
+  //strip punctuation from str and reverse it
+  let reverse = str
+    .toUpperCase()
+    .split("")
+    .filter((char) => {
+      return /[a-zA-Z0-9]/.test(char);
+    })
+    .reverse()
+    .join("");
 
+  let result = test.localeCompare(reverse);
+  return result == 0 ? true : false;
 };
 
-// Do not edit below this line
+// palindromes("Racecard!");
+// // // Do not edit below this line
 module.exports = palindromes;
