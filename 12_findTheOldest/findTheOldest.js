1be9ff4f1bb707ac768d6a186c51ed83a576d0c9
gushags <jeff@hagkull.com>
2025-03-04 14:30:23 -0800

    Passes three tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+function getAge(death, birth) {
+  if (!death) {
+    death = new Date().getFullYear();
+  }
+  return death - birth;
+}
 
+const findTheOldest = function (people) {
+  const person = people.sort(
+    (oldest, currentPerson) =>
+      getAge(currentPerson.yearOfDeath, currentPerson.yearOfBirth) -
+      getAge(oldest.yearOfDeath, oldest.yearOfBirth)
+  );
+
+  return person[0];
 };
 
 // Do not edit below this line
