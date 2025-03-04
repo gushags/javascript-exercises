8e3b04ba088e3068494aaef86dba7d13ee4a956c
gushags <jeff@hagkull.com>
2025-03-04 13:13:53 -0800

    Passes all tests -- ex 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  let index = Number(num);
+  if (index == 0) {
+    return 0;
+  } else if (index < 0) {
+    return "OOPS";
+  }
+  let fibArr = [];
+  for (let i = 0; i < index; i++) {
+    if (i == 0 || i == 1) {
+      fibArr[i] = 1;
+    } else {
+      fibArr[i] = fibArr[i - 2] + fibArr[i - 1];
+    }
+  }
+  return fibArr[index - 1];
 };
 
 // Do not edit below this line
