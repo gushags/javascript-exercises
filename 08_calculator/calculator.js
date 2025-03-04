6448075f5e5026ad51c57ea4b59ec4569698de20
gushags <jeff@hagkull.com>
2025-03-04 11:05:49 -0800

    Passes all tests -- exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  let result = arr.reduce((total, current) => total + current, 0);
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  let result = arr.reduce((total, current) => total * current, 1);
+  return result;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  let total = 1;
+  for (let i = 0; i < b; i++) {
+    total = total * a;
+  }
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+  let total = 1;
+  for (let i = 1; i < a + 1; i++) {
+    total = total * i;
+  }
+  return total;
 };
 
 // Do not edit below this line
@@ -29,5 +39,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
